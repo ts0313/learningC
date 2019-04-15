fb3368f7b97eaf4ddd5cd7894584b937bbf3326a
Nian <48542534+ts0313@users.noreply.github.com>
2019-04-15 23:50:56 +0800

    提交代码

---
+++ luogu/p1001/p1001/test.c
@@ -0,0 +1,12 @@
+//输入两个整数a,b，输出它们的和(|a|,|b|<=10^9)。
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d\n", a + b);
+	system("pause");
+	return 0;
+}
