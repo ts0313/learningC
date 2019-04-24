4f3f6786d2bd4783d75434c373e0b10709f02ae0
Nian <48542534+ts0313@users.noreply.github.com>
2019-04-24 23:33:52 +0800

    顺序表 头文件

---
+++ 2019_4_23/2019_4_23/SeqList.h
@@ -0,0 +1,47 @@
+#pragma once
+
+typedef int DataType;
+typedef struct SeqList
+{
+	DataType* _array;
+	int _capacity; // 顺序表的总大小
+	int _size; // 顺序表中有效元素的个数
+}SeqList, *PSeq;
+
+
+//初始化
+void SeqListInit(PSeq ps, int capacity);
+//尾插
+void SeqListPushBack(PSeq ps, DataType data);
+//删除尾部元素
+void SeqListPopBack(PSeq ps);
+//头插
+void SeqListPushFront(PSeq ps, DataType data);
+//删除头部元素
+void SeqListPopFront(PSeq ps);	
+//在顺序表pos位置插入值为data的元素
+void SeqListInsert(PSeq ps, int pos, DataType data);
+//删除pos位置的元素
+void SeqListErase(PSeq ps, int pos);
+//在顺序表中查找值为data的元素，找到返回该元素在顺序表中的下标，否则返回-1
+int SeqListFind(PSeq ps, DataType data);
+//返回顺序表中有效元素的个数
+int SeqListSize(PSeq ps);
+//返回顺序表的容量大小
+int SeqListCapacity(PSeq ps);
+//检测顺序表是否为空，如果为空返回非0值，非空返回0
+int SeqListEmpty(PSeq ps);
+//将顺序表中的元素清空
+void SeqListClear(PSeq ps);
+//移除第一个值为data的元素
+void SeqListRemove(PSeq ps, DataType data);
+//移除所有值为data的元素
+void SeqListRemoveAll(PSeq ps, DataType data);
+//销毁顺序表
+void SeqListDestory(PSeq ps);
+//顺序表的扩容
+void CheckCapacity(PSeq ps);
+//打印顺序表
+void SeqListPrint(PSeq ps);
+//排序顺序表
+void BubbleSort(PSeq ps);
